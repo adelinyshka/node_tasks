329bf1f6c323664dfa3ab2ad72dd561c67d80fc8
Adel Dubinnikova <adelinyshka@gmail.com>
2022-12-17 20:00:52 +0300

    refactor: change imports & function parametres

--- src/helpers.js
+++ src/helpers.js
@@ -1,21 +1,20 @@
-import { env } from "process";
 
 const getUserNameFromCli = (cliArguments) => {
     let userName;
     if (cliArguments) {
         userName = cliArguments.filter((i) => i.startsWith('--username')).join().slice(11);
-    } else { 
-        userName = 'Anoniymous'; 
+    } else {
+        userName = 'Anoniymous';
     }
     return userName;
 };
 
 const welcomeUser = (name) => {
     console.log(`Welcome to the File Manager, ${name}!`);
-}
+};
 
-const showUserPath = () => {
-    console.log(`You are currently in ${env.Home}`);
-}
+const showUserPath = (path) => {
+    console.log(`You are currently in ${path}`);
+};
 
 export { getUserNameFromCli, welcomeUser, showUserPath };
--- start.js
+++ start.js
@@ -1,9 +1,10 @@
 import { getUserNameFromCli, showUserPath, welcomeUser } from './src/helpers.js';
 import { argv } from 'process';
+import { env } from 'process';
 
 function startFM() {
   welcomeUser(getUserNameFromCli(argv));
-  showUserPath();
+  showUserPath(env.Home);
 }
 
 startFM();
